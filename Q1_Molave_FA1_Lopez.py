1b1dfedbd05d5154a7fb0a583b7b51b73a062624
rgolopez31-afk <rgolopez31@src.pshs.edu.ph>
2026-08-15 10:42:16 +0800

    Add files via upload

--- Q1_Molave_FA1_Lopez.py
+++ Q1_Molave_FA1_Lopez.py
@@ -1,13 +1,18 @@
 import math
-# This part of the code asks the user on the coordinates of the poins
+
+# Ask the user for the coordinates of the points
 x1 = float(input("What is the x coordinate of the first point? "))
 y1 = float(input("What is the y coordinate of the first point? "))
 x2 = float(input("What is the x coordinate of the second point? "))
 y2 = float(input("What is the y coordinate of the second point? "))
+
 x_sub = x2 - x1
 y_sub = y2 - y1
-# This part of the code
+
+# Do the formula
 ans = math.sqrt(pow(x_sub, 2) + pow(y_sub, 2))
-# This part of the code prints the answer
+
+# Print the answer
 print(f"The distance between ({x1}, {y1}) and ({x2}, {y2}) is {ans}")
-#Using a library instead of writing code from scratch is useful for many reasons. First, libraries make code shorter. Would you rather write lines of code just to perform one function, or import the math module to keep your code concise? Second, libraries are more reliable. If you write code from scratch, there is a chance it will produce incorrect results, whereas imported libraries are highly accurate. In conclusion, importing a library is better than building code from scratch because it ensures your programs are both shorter and more precise.#
+
+#Using a library instead of writing code from scratch is useful for many reasons. First, libraries make code shorter. Would you rather write lines of code just to perform one function, or import the math module to keep your code concise? Second, libraries are more reliable. If you write code from scratch, there is a chance it will produce incorrect results, whereas imported libraries are highly accurate. In conclusion, importing a library is better than building code from scratch because it ensures your programs are both shorter and more precise.
